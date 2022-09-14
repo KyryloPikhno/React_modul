25f77be4bc71a4664d0ceadcac1d4e08b723678e
KyryloPikhno <Mr.Good@i.ua>
2022-09-15 02:10:52 +0300

    my commit

--- src/components/person/person.js
+++ src/components/person/person.js
@@ -3,9 +3,9 @@ export default function Person(props){
 
     return(<div className={'box'}>
         <h3 className={'text'}>{person.name}</h3>
-        <p className={'text'}>surname -{person.surname}</p>
+        <p className={'text'}>surname - {person.surname}</p>
         <p className={'text'}>age - {person.age}</p>
         <p className={'text'}>{person.info}</p>
-        <img className={'img'} src={person.photo}/>
+        <img className={'pic'} src={person.photo} alt={'pic'}/>
     </div>);
 }
--- src/components/personComponents/Persons.js
+++ src/components/personComponents/Persons.js
@@ -2,7 +2,7 @@ import {simpsons} from "../data";
 import Person from "../person/person";
 
 export default function Persons(){
-    return(<div>
+    return(<div className={'container'}>
         {simpsons.map((person,index)=>(<Person person={person} key={index}/>))}
     </div>)
 }
