95c01b3ca5f1726bef3cf20b68a3b1ba477778b3
KyryloPikhno <Mr.Good@i.ua>
2022-09-16 19:33:39 +0300

    my commit

--- src/components/RickAndMortyComponents/Characters.js
+++ src/components/RickAndMortyComponents/Characters.js
@@ -1,15 +1,16 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Character from '../character/character'
 
 export default function Characters(){
     // eslint-disable-next-line react-hooks/rules-of-hooks
     let[characters,setCharacters] = useState([])
 
+    useEffect(()=>{
     fetch('https://rickandmortyapi.com/api/character')
         .then(value => value.json())
         .then(value => {
             setCharacters(value.results.splice(0,6))
-        })
+        })},[])
 
 return (
     <div className={'container'}>
