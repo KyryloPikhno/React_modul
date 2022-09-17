3623b502804ce2d6a6c407453817f28d90498bb4
KyryloPikhno <Mr.Good@i.ua>
2022-09-17 17:30:01 +0300

    my commit

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,6 +1,6 @@
 
 export default function User(props){
-    let{user} = props
+    let{user,lift} = props
 
     return(
         <div className={'box'}>
@@ -8,6 +8,10 @@ export default function User(props){
             <p>username: {user.username}</p>
             <a href={'mailto'}>{user.email}</a>
             <img className={'pic'} alt={'pic'} src={'https://drasler.ru/wp-content/uploads/2019/05/%D0%9A%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0-%D0%B0%D0%B2%D0%B0-%D0%B2-%D0%BE%D1%82%D0%BF%D1%83%D1%81%D0%BA%D0%B5-%D0%BF%D0%BE%D0%B4%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-021.jpg'}/>
+
+            <button onClick={()=>{
+                    lift(user)
+            }}>info</button>
         </div>
     )
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,6 +3,11 @@ import User from '../user/User'
 
 export default function Users(){
     let[users,setUsers]= useState([])
+    let[user,setUser]= useState(null)
+
+       let lift=(obj) => {
+            setUser(obj)
+        }
 
     useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -10,6 +15,10 @@ export default function Users(){
         .then(value => setUsers(value.splice(0,8)))
     },[])
     return(<div className={'container'}>
-        {users.map((user,index)=> (<User user={user} key={index}/>))}
+        <div className={'box'}>
+           <p>{'id:'} {user?.id}</p>
+            <p>{'phone:'} <a href={'user?.phone'}>{user?.phone}</a></p>
+        </div>
+        {users.map((user,index)=> (<User user={user} key={index} lift={lift}/>))}
     </div>)
 }
