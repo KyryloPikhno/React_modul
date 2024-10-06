5efee9f4ec6ba5b9fa69e35b82c4e0567d211662
KyryloPikhno <Mr.Good@i.ua>
2024-10-06 22:31:42 +0200

    fix compose

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const multiplyBy3 = (x) => x * 3;
   const subtract10 = (x) => x - 10;
 
-  const composedFunction = compose(subtract10, multiplyBy3, add5);
+  const composedFunction = compose(subtract10, subtract10, multiplyBy3, add5);
   const result = composedFunction(7);
 
   return (
